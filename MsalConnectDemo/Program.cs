078c7e9e78f56e24414716f36c087b2f6f862921
Rick Van Rousselt <rick@rivaro.be>
2016-11-16 19:02:00 +0100

    Add project files.

---
+++ MsalConnectDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace MsalConnectDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RunAsync().Wait();
+            Console.ReadKey();
+        }
+
+        static async Task RunAsync()
+        {
+            // Fill in the correct client ID for your app 
+            // Go to https://apps.dev.microsoft.com/ to register an app
+            const string clientId = "<GUID>";
+            // Enter the required scopes
+            string[] scopes = { "User.Read" };
+
+            // Create the application context.
+            var clientApplication = new PublicClientApplication(clientId);
+
+            // Acquire an access token for the given scope.
+            var authenticationResult = await clientApplication.AcquireTokenAsync(scopes);
+
+            // Write the access token.
+            Console.WriteLine(authenticationResult.Token);
+        }
+    }
+}
